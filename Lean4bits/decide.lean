648fae2573c9c615a80747372e3a304a3b6ffee7
ChrisHughes24 <chrishughes24@gmail.com>
2023-01-20 14:09:50 +0000

    add tests

--- Lean4bits/decide.lean
+++ Lean4bits/decide.lean
@@ -1,6 +1,6 @@
 import Lean4bits.lemmas
 
-open Fintype
+open Fintype Term
 
 instance decidableEvalEq (t₁ t₂ : Term) :
     Decidable (t₁.eval = t₂.eval) := by
@@ -32,3 +32,39 @@ instance decidableEvalEq (t₁ t₂ : Term) :
 
 def decide (t₁ t₂ : Term) : Bool :=
   t₁.eval = t₂.eval
+
+def x := Term.var 0
+def y := Term.var 1
+
+-- Checking if the operations satisfy the defining identities
+-- #eval decide (x + -x) 0
+-- #eval decide (incr x) (x + 1)
+-- #eval decide (decr x) (x - 1)
+-- #eval decide (x + - y) (x - y)
+-- #eval decide (neg_one) (-1)
+-- #eval decide (x + 0) (var 0)
+-- #eval decide (x + y) (y + x)
+
+-- -- Equalities from Zulip
+-- #eval decide (-x) (not x).incr
+-- #eval decide (-x) (not x.decr)
+-- #eval decide (not x) (-x).decr
+-- #eval decide (-not x) x.incr
+-- #eval decide (x + y) (x - not y).decr
+-- #eval decide (x + y) ((xor x y) + (and x y).ls)
+-- #eval decide (x + y) (or x y + and x y)
+-- #eval decide (x + y) ((or x y).ls - (xor x y))
+-- #eval decide (x - y) (x + not y).incr
+-- #eval decide (x - y) (xor x y - (and (not x) y).ls)
+-- #eval decide (x - y) (and x (not y) - (and (not x) y))
+-- #eval decide (x - y) ((and x (not y)).ls - (xor x y))
+-- #eval decide (xor x y) ((or x y) - (and x y))
+-- #eval decide (and x (not y)) (or x y - y)
+-- #eval decide (and x (not y)) (x - and x y)
+
+-- #eval decide (not (x - y)) (y - x).decr
+-- #eval decide (not (x - y)) (not x + y)
+-- #eval decide (not (xor x y)) (and x y - (or x y)).decr
+-- #eval decide (not (xor x y)) (and x y + not (or x y))
+-- #eval decide (or x y) (and x (not y) + y)
+-- #eval decide (and x y) (or (not x) y - not x)
